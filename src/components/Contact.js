18e1e99d03ca5ae549f26386a89536a24e292e05
leandro <leandro@isset.nl>
2018-10-18 11:02:54 +0200

    ëvents tested

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
  class Contact extends Component {
+     state = {
+
+     }
+     onShowClick = (id, name) => {
+         console.log(id);
+     }
   render() {
       const { name, email } = this.props.contact;
       //destructuring
     return (
       <div>
-        <h1>{name}</h1>
+        <h1>{name} <i onClick={this.onShowClick.bind(this, name)} className="fa fa-sort-down"></i></h1>
         <h2>{email}</h2>
       </div>
     )
